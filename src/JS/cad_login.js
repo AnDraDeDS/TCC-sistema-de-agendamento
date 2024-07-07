72cbb0445530bf16ea3892ec2ff36eed10379406
RHYANELY <rhyanelycostaesilva@gmail.com>
2024-07-07 18:25:02 -0300

    implementando função de esconder/mostrar senha e ajuste de layout

---
+++ src/JS/cad_login.js
@@ -0,0 +1,18 @@
+let eye_closed = document.getElementById("eye_closed");
+let eye_open = document.getElementById("eye_open");
+let senha = document.getElementById("senha");
+
+
+function olhar() {
+    eye_open.classList.add("d-none");
+    eye_closed.classList.remove("d-none");
+    senha.removeAttribute("type");
+    senha.setAttribute("type", "text");
+}
+
+function esconder() {
+    eye_closed.classList.add("d-none");
+    eye_open.classList.remove("d-none");
+    senha.removeAttribute("type");
+    senha.setAttribute("type", "password");
+}
