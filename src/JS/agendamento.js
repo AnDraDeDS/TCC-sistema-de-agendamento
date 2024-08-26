9a8e993291148e8728fb2cc5fa4607a5af0b29db
RHYANELY <rhyanelycostaesilva@gmail.com>
2024-08-26 18:58:31 -0300

    desenvolvimento da escolha de horarios em agendamneto

--- src/JS/agendamento.js
+++ src/JS/agendamento.js
@@ -1,10 +1,18 @@
 
 let content = document.querySelector(".content");
 let content2 = document.querySelector(".content_focus");
+let content3 = document.querySelector(".content_agenda");
 
 function servico_foco(){
     content.classList.add("d-none");
+    content3.classList.add("d-none");
     content2.classList.remove("d-none");
+}
+
+function agend_foco(){
+    content.classList.add("d-none");
+    content2.classList.add("d-none");
+    content3.classList.remove("d-none");
 }
 
 function voltar(){
