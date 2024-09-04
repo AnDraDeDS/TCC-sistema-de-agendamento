fde4a0ab47c995859542565ec5e39e525dce2e3a
RHYANELY <rhyanelycostaesilva@gmail.com>
2024-09-03 23:18:31 -0300

    introduzindo calendario JS

--- src/JS/agendamento.js
+++ src/JS/agendamento.js
@@ -2,6 +2,16 @@
 let content = document.querySelector(".content");
 let content2 = document.querySelector(".content_focus");
 let content3 = document.querySelector(".content_agenda");
+let button_mes = document.querySelector("#mes");
+let dropdown = document.querySelector(".dropdown-menu");
+
+function drop(){
+ dropdown.classList.toggle("d-none");
+}
+
+function hide(){
+ dropdown.classList.add("d-none");
+}
 
 function servico_foco(){
     content.classList.add("d-none");
@@ -20,25 +30,80 @@ function voltar(){
     content.classList.remove("d-none");
 }
 
-var dog = new Date()
-var day = dog.getDate();
+const header = document.querySelector(".calendar button");
+const navs = document.querySelectorAll(".prev, .next");
+const dates = document.querySelector(".dates");
 
-if(day < 38){
-    let numb = day;
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
 
-    while(numb < 38){
-        let dia = document.getElementById(`d${numb}`);
-        dia.innerHTML=`${numb}`;
-        numb++;
-    }
+let date = new Date();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+function renderCalendar() {
+  const start = new Date(year, month, 1).getDay();
+  const endDate = new Date(year, month + 1, 0).getDate();
+  const end = new Date(year, month, endDate).getDay();
+  const endDatePrev = new Date(year, month, 0).getDate();
+
+  let datesHtml = "";
+
+  for (let i = start; i > 0; i--) {
+    datesHtml += `<li class="inactive">${endDatePrev - i + 1}</li>`;
+  }
+
+  for (let i = 1; i <= endDate; i++) {
+    let className =
+      i === date.getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear()
+        ? ' class="today"'
+        : "";
+    datesHtml += `<li${className}>${i}</li>`;
+  }
+
+  for (let i = end; i < 6; i++) {
+    datesHtml += `<li class="inactive">${i - end + 1}</li>`;
+  }
+
+  dates.innerHTML = datesHtml;
+  header.textContent = `${months[month]} ${year}`;
+  dropdown.textContent = `${months[month+1]} ${year}`;
 }
 
-if(day > 1){
-    let numb2 = day;
+navs.forEach((nav) => {
+  nav.addEventListener("click", (e) => {
+    const btnId = e.target.id;
 
-    while(numb2 > 1){
-        let dia = document.getElementById(`d${numb2}`);
-        dia.innerHTML=`${numb2}`;
-        numb2--;
+    if (btnId === "prev" && month === 0) {
+      year--;
+      month = 11;
+    } else if (btnId === "next" && month === 11) {
+      year++;
+      month = 0;
+    } else {
+      month = btnId === "next" ? month + 1 : month - 1;
     }
-}
+
+    date = new Date(year, month, new Date().getDate());
+    year = date.getFullYear();
+    month = date.getMonth();
+
+    renderCalendar();
+  });
+});
+
+renderCalendar();
