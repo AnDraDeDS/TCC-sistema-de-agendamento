419c445bec5e99268aeecb086741fa4dde9fea0d
RHYANELY <rhyanelycostaesilva@gmail.com>
2024-09-04 09:37:19 -0300

    resolvendo problemão do calendário

--- src/JS/agendamento.js
+++ src/JS/agendamento.js
@@ -5,8 +5,40 @@ let content3 = document.querySelector(".content_agenda");
 let button_mes = document.querySelector("#mes");
 let dropdown = document.querySelector(".dropdown-menu");
 
-function drop(){
+const header = document.querySelector(".calendar button");
+const navs = document.querySelectorAll("#prev, #next");
+const dates = document.querySelector(".dates");
+
+function atual(){
  dropdown.classList.toggle("d-none");
+ if(dropdown.getAttribute("id") != "next"){
+  dropdown.setAttribute("id", "next");
+ }else{
+  dropdown.setAttribute("id", "prev");
+ }
+}
+
+function atualizar(){
+
+  const btnId = dropdown.getAttribute("id"); 
+  if (btnId === "prev" && month === 0) {
+    year--;
+    month = 11;
+    } else if (btnId === "next" && month === 11) {
+    year++;
+    month = 0;
+    } else {
+    month = btnId === "next" ? month + 1 : month - 1;
+    }
+
+    date = new Date(year, month, new Date().getDate());
+    year = date.getFullYear();
+    month = date.getMonth();
+
+    console.log(year, month);
+    dropdown.classList.toggle("d-none");
+
+    renderCalendar();
 }
 
 function hide(){
@@ -30,9 +62,6 @@ function voltar(){
     content.classList.remove("d-none");
 }
 
-const header = document.querySelector(".calendar button");
-const navs = document.querySelectorAll(".prev, .next");
-const dates = document.querySelector(".dates");
 
 const months = [
   "Janeiro",
@@ -81,7 +110,12 @@ function renderCalendar() {
 
   dates.innerHTML = datesHtml;
   header.textContent = `${months[month]} ${year}`;
-  dropdown.textContent = `${months[month+1]} ${year}`;
+  if(dropdown.getAttribute("id") == "next"){
+    dropdown.textContent = `${months[month-1]} ${year}`; // Resolver virada do ano
+  }else{
+    dropdown.textContent = `${months[month+1]} ${year}`; //
+  }
+  
 }
 
 navs.forEach((nav) => {
@@ -102,8 +136,42 @@ navs.forEach((nav) => {
     year = date.getFullYear();
     month = date.getMonth();
 
+    console.log(year, month);
     renderCalendar();
   });
+
 });
 
 renderCalendar();
+
+
+function renderCalendar2(ano, mes) {
+  const start = new Date(ano, mes, 1).getDay();
+  const endDate = new Date(ano, mes + 1, 0).getDate();
+  const end = new Date(ano, mes, endDate).getDay();
+  const endDatePrev = new Date(ano, mes, 0).getDate();
+
+  let datesHtml = "";
+
+  for (let i = start; i > 0; i--) {
+    datesHtml += `<li class="inactive">${endDatePrev - i + 1}</li>`;
+  }
+
+  for (let i = 1; i <= endDate; i++) {
+    let className =
+      i === date.getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear()
+        ? ' class="today"'
+        : "";
+    datesHtml += `<li${className}>${i}</li>`;
+  }
+
+  for (let i = end; i < 6; i++) {
+    datesHtml += `<li class="inactive">${i - end + 1}</li>`;
+  }
+
+  dates.innerHTML = datesHtml;
+  header.textContent = `${months[month]} ${year}`;
+  dropdown.textContent = `${months[month+1]} ${year}`;
+}
